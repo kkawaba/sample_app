14f04a5ef02109ef8ba10d6e4fb6c8be2de5800a
Kawabata Kuon <kkawabata@example.com>
2022-11-28 12:39:00 +0000

    Add a micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -49,21 +50,12 @@ class UsersController < ApplicationController
   
   private
   
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-  end
-  
-  # beforeフィルタ
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
     end
+  
+    # beforeフィルタ
     
     # 正しいユーザーかどうか確認
     def correct_user
